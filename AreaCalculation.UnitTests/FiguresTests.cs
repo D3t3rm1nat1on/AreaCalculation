d7e6890411b0807727371c3b8d5aed6be8d5f1f4
D3t3rm1nat1on <bro.chek@mail.ru>
2020-04-29 09:49:08 +0300

    Initial commit

---
+++ AreaCalculation.UnitTests/FiguresTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AreaCalculation.UnitTests
+{
+    [TestFixture]
+    public class FiguresCalculateSquareTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var rectangleSquare = new Triangle(5, 5 * Math.Sqrt(2), 5).CalculateSquare();
+            Assert.AreEqual(12.5, rectangleSquare);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var circleSquare = new Circle(1.2).CalculateSquare();
+            Assert.AreEqual(4.524, circleSquare, 0.001);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Exception exception = null;
+            try
+            {
+                var circleSquare = new Circle(-1.2).CalculateSquare();
+            }
+            catch (FigureException ex)
+            {
+                exception = ex;
+            }
+
+            Assert.True(exception is FigureException);
+        }
+        
+        [Test]
+        public void Test4()
+        {
+            Exception exception = null;
+            try
+            {
+                var rectangleSquare = new Triangle(-1,3,15).CalculateSquare();
+            }
+            catch (FigureException ex)
+            {
+                exception = ex;
+            }
+
+            Assert.True(exception is FigureException);
+        }
+        
+        
+        [Test]
+        public void Test5()
+        {
+            var isRectangular = new Triangle(12.2, 12.421, 17.410377393956743).IsRectangular();
+            Assert.True(isRectangular);
+        }
+        
+        
+    }
+}
---
+++ AreaCalculation/Figures.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace AreaCalculation
+{
+    public abstract class Figure
+    {
+       public abstract double CalculateSquare();
+    }
+
+    public class FigureException : Exception
+    {
+        public FigureException(string message)
+            : base(message)
+        {
+        }
+    }
+
+    public class Triangle : Figure
+    {
+        private double _a, _b, _c;
+        private double _p;
+
+        public override double CalculateSquare()
+        {
+            _p = (_a + _b + _c) / 2;
+            return Math.Sqrt(_p * (_p - _a) * (_p - _b) * (_p - _c));
+        }
+
+        public bool IsRectangular()
+        {
+            double tolerance = 0.00001;
+            if (_c > _b && _c > _a)
+                return Math.Abs(_c * _c - (_a * _a + _b * _b)) < tolerance;
+            if (_b > _c && _b > _a)
+                return Math.Abs(_b * _b - (_a * _a + _c * _c)) < tolerance;
+            return Math.Abs(_a * _a - (_c * _c + _b * _b)) < tolerance;
+        }
+
+        public Triangle(double a, double b, double c)
+        {
+            if (a + b > c && a + c > b && b + c > a && a > 0 && b > 0 && c > 0)
+            {
+                _a = a;
+                _b = b;
+                _c = c;
+            }
+            else
+                throw new FigureException($"There is no triangle with such sides: {a}, {b}, {c}");
+        }
+    }
+
+    public class Circle : Figure
+    {
+        private double _radius;
+
+        public override double CalculateSquare()
+        {
+            return Math.PI * _radius * _radius;
+        }
+
+        public Circle(double radius)
+        {
+            if (radius > 0)
+                _radius = radius;
+            else
+                throw new FigureException($"There is no circle with this radius: {radius}");
+        }
+    }
+}
